5ec41204b7cf4d40bf4a88aaed1e2aa8bc0fbbc7
Alex Seitz <alex.seitz1@gmx.net>
2023-01-05 13:44:48 +0100

    Setup for documentation

--- docs/make.jl
+++ docs/make.jl
@@ -1,25 +1,12 @@
-using PatentsLandscapes
-using Documenter
+using Documenter, PatentsLandscapes
 
-DocMeta.setdocmeta!(PatentsLandscapes, :DocTestSetup, :(using PatentsLandscapes); recursive=true)
-
-makedocs(;
-    modules=[PatentsLandscapes],
-    authors="Alex Seitz <alex.seitz1@gmx.net> and contributors",
-    repo="https://github.com/Seitzal/PatentsLandscapes.jl/blob/{commit}{path}#{line}",
-    sitename="PatentsLandscapes.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://Seitzal.github.io/PatentsLandscapes.jl",
-        edit_link="default",
-        assets=String[],
-    ),
-    pages=[
-        "Home" => "index.md",
-    ],
+makedocs(
+    modules = [PatentsLandscapes],
+    sitename = "PatentsLandscapes.jl",
+    format = Documenter.HTML(; prettyurls = get(ENV, "CI", nothing) == "true")
 )
 
-deploydocs(;
-    repo="github.com/Seitzal/PatentsLandscapes.jl",
-    devbranch="default",
+deploydocs(
+    repo = "github.com/JuliaPatents/PatentsLandscapes.jl.git",
+    devbranch = "main"
 )
