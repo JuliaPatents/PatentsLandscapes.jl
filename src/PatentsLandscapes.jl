2b187dfcb040a09859532ed994e2819b320bf000
Alex Seitz <alex.seitz1@gmx.net>
2023-01-19 14:06:07 +0100

    Move data levels ADT to PatentsBase

--- src/PatentsLandscapes.jl
+++ src/PatentsLandscapes.jl
@@ -7,9 +7,6 @@ using PatentsBase
 # Custom taxonomies
 export define_taxon!, TaxonomicFilter
 
-# Analysis levels
-export AnalysisLevel, Applications, Families
-
 # Grouping variables
 export Grouping, Applicants, Jurisdictions, TimeResolution, Years, Months, TimeTrend,
     Taxonomy
@@ -21,7 +18,6 @@ export Analysis, Frequency
 export prepdata
 
 include("taxonomies.jl")
-include("levels.jl")
 include("groupings.jl")
 include("analyses.jl")
 
--- src/analyses.jl
+++ src/analyses.jl
@@ -6,7 +6,15 @@ abstract type Analysis end
 struct Frequency <: Analysis end
 
 """
-    prepdata(ds::AbstractDataSource, type::Analysis, level::AnalysisLevel, [filter::AbstractFilter], [g1::Grouping], [g2::Grouping], [g3::Grouping])::DataFrame
+    prepdata(
+        ds::AbstractDataSource,
+        type::Analysis,
+        level::AnalysisLevel,
+        [filter::AbstractFilter],
+        [g1::Grouping],
+        [g2::Grouping],
+        [g3::Grouping]
+    )::DataFrame
 
 Perform data retrieval and calculation for an analysis, yielding the results as a `DataFrame`.
 
@@ -14,7 +22,7 @@ Parameters (in order):
 
 * `ds`: The `AbstractDataSource` containing the data for the analysis.
 * `type`: The type of `Analysis` to be conducted.
-* `level`: The `AnalysisLevel` for the analysis, e.g. `Applications()`, `Families()` etc.
+* `level`: The `DataLevel` for the analysis, e.g. `ApplicationLevel()`, `FamilyLevel()` etc.
 * `filter` (optional): A filter defining the subset of data to be included in the analysis.
     If not specified, all data stored in `ds` is included.
 * `g1` - `g3` (optional): Up to three `Grouping` categories to use in the analysis.
